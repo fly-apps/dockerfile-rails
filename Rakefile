6778d5d2f95c064723cb019b9f124457b28e579f
Sam Ruby <rubys@intertwingly.net>
2023-01-19 17:40:46 -0500

    first test

--- Rakefile
+++ Rakefile
@@ -1,3 +1,10 @@
 require "bundler/gem_tasks"
 
 # Run `rake release` to release a new version of the gem.
+
+require 'rake/testtask'
+Rake::TestTask.new do |t|
+  t.libs << "test"
+  t.test_files = FileList['test/test*.rb']
+  t.verbose = true
+end
---
+++ test/test_minimal.rb
@@ -0,0 +1,24 @@
+require 'test/unit'
+
+class TestMinimal < Test::Unit::TestCase
+  def test_minimal
+    Dir.chdir 'test/tmp' do
+      system 'rails new --minimal minimal'
+
+      Dir.chdir 'minimal' do
+        system 'bundle add dockerfile-rails --group development'
+        system 'bin/rails generate dockerfile'
+      end
+
+      results = IO.read('minimal/Dockerfile')
+        .gsub(/(^ARG\s+\w+\s*=).*/, '\1')
+
+      expected = IO.read('../results/minimal/Dockerfile')
+        .gsub(/(^ARG\s+\w+\s*=).*/, '\1')
+      
+      assert_equal expected, results
+    end
+  ensure
+    FileUtils.rm_rf 'test/tmp/minimal'
+  end
+end

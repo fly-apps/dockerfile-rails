d249ffbfc91b8a6ffaf3991d7e0c85cb97a2c9fa
Sam Ruby <rubys@intertwingly.net>
2023-03-05 10:26:33 -0500

    retire ci workaround once Ruby 3.2.2 ships

--- lib/generators/dockerfile_generator.rb
+++ lib/generators/dockerfile_generator.rb
@@ -369,7 +369,9 @@ private
     gems = ["bundler"]
 
     if options.ci? && options.lock? && @gemfile.include?("debug")
-      gems += %w(irb reline) - @gemfile
+      # https://github.com/rails/rails/pull/47515
+      # https://github.com/rubygems/rubygems/issues/6082#issuecomment-1329756343
+      gems += %w(irb reline) - @gemfile unless Gem.ruby_version >= '3.2.2'
     end
 
     gems.sort

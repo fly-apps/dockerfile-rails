40212eed4549a598d39f4289ecfac4b4ad158628
Sam Ruby <rubys@intertwingly.net>
2023-08-04 12:13:11 +0200

    fix docker compose

--- test/test_private_gemserver.rb
+++ test/test_private_gemserver.rb
@@ -4,10 +4,11 @@ require_relative "base"
 
 class TestPrivateGemserver < TestBase
   @rails_options = "--minimal"
-  @generate_options = "--private-gemserver-domain gems.example.com"
+  @generate_options = "--compose --private-gemserver-domain gems.example.com"
 
   def test_private_gemserver
     check_dockerfile
     check_dockerignore
+    check_compose
   end
 end
--- test/test_private_gemserver_with_cache.rb
+++ test/test_private_gemserver_with_cache.rb
@@ -4,10 +4,11 @@ require_relative "base"
 
 class TestPrivateGemserverWithCache < TestBase
   @rails_options = "--minimal"
-  @generate_options = "--cache --private-gemserver-domain gems.example.com"
+  @generate_options = "--compose --cache --private-gemserver-domain gems.example.com"
 
   def test_private_gemserver_with_cache
     check_dockerfile
     check_dockerignore
+    check_compose
   end
 end

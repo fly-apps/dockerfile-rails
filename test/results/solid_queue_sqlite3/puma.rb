ff53b0357bcc606afab8004c17b8691b2d3bd387
Sam Ruby <rubys@intertwingly.net>
2024-11-04 14:24:44 -0500

    Set SOLID_QUEUE_IN_PUMA if present

--- test/results/solid_queue_sqlite3/puma.rb
+++ test/results/solid_queue_sqlite3/puma.rb
@@ -39,6 +39,3 @@ plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
-
-# Run the Solid Queue's supervisor
-plugin :solid_queue
